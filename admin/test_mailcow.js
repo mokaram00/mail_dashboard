d0ea4f9f1903496250b1fefa0a7f43ab283795eb
Mokaram Alzein <mokaramalzein26@gmail.com>
2025-12-17 03:29:22 +0200

    123

--- admin/test_mailcow.js
+++
@@ -1,9 +0,0 @@
-const { createMailbox, deleteMailbox, getMailboxes, getDomains, updateMailboxQuota } = require('./mailcow');
-
-console.log('Mailcow module loaded successfully!');
-console.log('Available functions:');
-console.log('- createMailbox');
-console.log('- deleteMailbox');
-console.log('- getMailboxes');
-console.log('- getDomains');
-console.log('- updateMailboxQuota');
